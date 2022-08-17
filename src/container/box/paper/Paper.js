4e1d8f20c797c6999c04bc24a514e4ce56a3f362
sblgrfl@gmail.com <sblgrfl@gmail.com>
2022-08-17 10:17:09 +0200

    layout complete

--- src/container/box/paper/Paper.js
+++ src/container/box/paper/Paper.js
@@ -1,10 +1,32 @@
 import './Paper.css';
 import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+
 
 class Paper extends React.Component {
     render() {
         return (
-            <div className='Paper'></div>
+            <div className='Paper'>
+                <Card sx={{ minWidth: 275 }}>
+                    <CardContent>
+                        <Typography variant="h5" component="div">
+                            Search for Cities
+                        </Typography>
+                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                            Start edit the text below to find an available City
+                        </Typography>
+                        <Autocomplete
+                            id="controllable-states-demo"
+                            sx={{ width: 300 }}
+                            renderInput={(params) => <TextField {...params} label="Controllable" />}
+                        />
+                    </CardContent>
+                </Card>
+            </div>
         )
     }
 }
