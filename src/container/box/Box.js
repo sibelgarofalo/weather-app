526b6541a7038bed6bb7088a6c7f9438d5d69af9
sblgrfl@gmail.com <sblgrfl@gmail.com>
2022-08-17 09:30:53 +0200

    i comleted the toolbar

--- src/container/box/Box.js
+++ src/container/box/Box.js
@@ -1,13 +1,13 @@
 import './Box.css';
 import React from 'react';
-import Toolbar from './toolbar/Toolbar';
+import ToolbarComponent from './toolbar/ToolbarComponent';
 import Paper from './paper/Paper';
 
 class Box extends React.Component {
     render() {
         return (
             <div className='Box'>
-                <Toolbar/>
+                <ToolbarComponent/>
                 <Paper/>
             </div>
         )
